faa1cc2e4641474f0087892443f43ce83633979b
sergio <systemsistec@gmail.com>
2026-06-09 15:34:30 -0300

    Header general

---
+++ modulot/src/components/HeaderGeneral.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Navbar, Nav, Dropdown, Container, NavDropdown } from 'react-bootstrap'
+
+export default function HeaderGeneral() {
+
+            {/*            
+            <Navbar>                    Barra de navegación 
+            <Navbar.Brand>              Logo 
+            <Navbar.Toggle>             Botón hamburguesa 
+            <Navbar.Collapse>           Contenedor del menú 
+            <Nav>                       Contenedor de items 
+            <Nav.Link>                  Links del menú 
+            <Dropdown>                  Menú desplegable 
+            <Dropdown.Toggle>           Botón que abre 
+            <Dropdown.Menu>             Menú desplegable 
+            <Dropdown.Item>             Items del dropdown 
+            <Dropdown.Header>           Encabezado 
+            <Dropdown.Divider>          Separador                    
+            */}
+
+    return (
+       
+     //menu principal
+     <header>
+        <Navbar expand="lg" className='bg-warning'> 
+            <Container>
+                <Navbar.Brand href="#">SystemDesarrollo</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#">Home</Nav.Link>
+                    <Nav.Link href="#">Sistemas</Nav.Link>
+                    <Nav.Link href="#">Ventas</Nav.Link>                    
+                    <NavDropdown title="Servicios" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#">Soporte de Sistemas</NavDropdown.Item>                    
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#">Soporte de Equipos</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#">Contacto</Nav.Link> 
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>            
+    </header>   
+            
+    )   
+}
