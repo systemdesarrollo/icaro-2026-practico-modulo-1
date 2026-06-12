1caed563e2bde3a663e431d1a4b992bda6abc068
sergio <systemsistec@gmail.com>
2026-06-12 12:52:29 -0300

    Formulario de contactos

---
+++ modulot/src/pages/ContactoFormulario.jsx
@@ -0,0 +1,19 @@
+import React,{ useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";   // destructuro
+
+
+export default function ContactoFormulario() {
+
+    return (
+        
+        <Container>
+            <Row>
+                <Col className="col-md-12 text-center bg-body-secondary p-5 align-content-center">
+                 
+                
+                
+                </Col>
+            </Row>
+        </Container>
+    )
+}
