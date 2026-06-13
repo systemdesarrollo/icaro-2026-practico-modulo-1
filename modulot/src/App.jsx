f66fcbeb3634ad4476dc99d8320cc5920b9ba903
sergio <systemsistec@gmail.com>
2026-06-13 15:25:27 -0300

    app

--- modulot/src/App.jsx
+++ modulot/src/App.jsx
@@ -4,24 +4,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 import HeaderGeneral from './components/HeaderGeneral';
-import MainFirst from './components/MainFirst';
-import MainSecond from './components/MainSecond';
+import MainFirst from './pages/MainFirst';
+import MainSecond from './pages/MainSecond';
 import FooterGeneral from './components/FooterGeneral';
+import BannerPublicidad from './components/BannerPublicidad';
+import Layouts from './components/Layouts';
 
-import BannerPublicidad from './context/BannerPublicidad';
+//import Layouts from './components/Layouts';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <HeaderGeneral/>
-       <BannerPublicidad/>
+  
+    
+      <Layouts>
+        <BannerPublicidad/>
 
-       <MainFirst/>
-       <MainSecond/>
+        <MainFirst/>
+        <MainSecond/>
 
-      <FooterGeneral/>
+      </Layouts>
+  
     </>
   )
 }
